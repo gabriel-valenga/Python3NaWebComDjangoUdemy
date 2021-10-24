32de0bc898068bcd41f7b2a54e96265fb51b6077
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2021-10-24 12:18:26 -0300

    Criado app courses e model Course

---
+++ simplemooc/courses/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Courses(models.Model):
+    name = models.CharField(verbose_name='Nome', max_length=100)
+    slug = models.SlugField(verbose_name='Atalho')
+    description = models.TextField('Descrição', blank=True)
+    start_date = models.DateField(verbose_name='Data de ínicio',
+                                  null=True, blank= True)
+    image = models.ImageField(upload_to='courses/images',
+                              verbose_name='imagem',
+                              null=True, blank=True)
+    created_at = models.DateTimeField(verbose_name='Criado em',
+                                      auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Atualizado em',
+                                      auto_now=True)
--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'simplemooc.core'
+    'simplemooc.core',
+    'simplemooc.courses'
 ]
 
 MIDDLEWARE = [
@@ -120,8 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
